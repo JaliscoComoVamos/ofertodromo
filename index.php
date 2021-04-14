6b5dcbe9ef317a60d7783903e5aad59aae482b28
jandrajandra <jan.tr9@gmail.com>
2021-04-14 10:31:47 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php include('includes/header.php'); ?>
+<!-- cover -->
+<div class="ofertodromo-back mt20">
+    <div class="container">
+        <div class="row text-center">
+            <img class="logo-intro" src="http://jaliscocomovamos.org/ofertodromo/img/indicador-ofertodromo.svg">
+        </div>
+    </div>
+</div>
+<div class="about" id="#about">
+    <div class="container mt80">
+        <div class="row mb20 mt80" id="acerca-de">
+            <div class="col-md-12 col-xs-12">
+                <div class="text-center">
+                    <h1>¡Es temporada de ofertas!</h1>
+                    <h2>Conoce las promesas de campaña de las y los candidatos a distintos cargos en estas elecciones.</h2>
+                    <h2>El #Ofertódromo2018 es un proyecto de <a target="_blank" href="http://jaliscocomovamos.org">Jalisco Cómo Vamos</a>, un observatorio de calidad de vida.</h2> </div>
+                </div>
+            </div>
+            <div class="row" id="acerca-de">
+                <div class="text-center mt80 mb20">
+                <h1>Busca las ofertas</h1></div>
+                <div class="col-md-3 col-xs-12">
+                    <div class="text-center">
+                        <h2 class="hp" style="font-weight:800;">Elige</h2>
+                    <h4>Aquí encontrarás las ofertas y promesas de campaña de los candidatos a la presidencia de México y de los candidatos de diputados locales y federales, senadores, presidentes municipales y gobernador de Jalisco. Elige el cargo que sea de tu interés en el menú superior o en los botones que encontrarás debajo de esta sección.</h4> </div>
+                </div>
+                <div class="col-md-3 col-xs-12">
+                    <div class="text-center">
+                        <h2 class="hp" style="font-weight:800;">Filtra</h2>
+                        <h4>Todas las ofertas están ordenadas por candidatos (o distritos, si aplica) o por categorías de ejes temáticos. Elige dentro de estos filtros para agilizar tu consulta. Si no conoces que distrito te corresponde, puedes consultarlo en el sitio de <a target="_blank" href="https://jaliscocomovotamos.netlify.com/"><strong>Jalisco Cómo Votamos</strong></a></h4> </div>
+                    </div>
+                    <div class="col-md-3 col-xs-12">
+                        <div class="text-center">
+                            <h2 class="hp" style="font-weight:800;">Consulta</h2>
+                            <h4>Todas las ofertas están sustentadas por el archivo pdf o grabación directa de los medios que las difundieron; por razones de derechos de autor no las podemos circular. Sin embargo, si te interesa revisar los testigos en los que se basa nuestro Ofertódromo, <a target="_blank" href="http://jaliscocomovamos.org/contactanos-2">dirígete a nosotros</a>.</h4> </div>
+                        </div>
+                        <div class="col-md-3 col-xs-12">
+                            <div class="text-center">
+                                <h2 class="hp" style="font-weight:800;">Analiza</h2>
+                                <h4>Te recomendamos que al revisar las ofertas de campaña realices un ejercicio de reflexión. Pregúntate:
+                                <br>¿Es relevante la propuesta?
+                                <br>¿Se refiere a una problemática prioritaria a atender?
+                                <br>¿Es de su competencia?
+                                <br>¿Es presupuestalmente viable?
+                                <br>¿Es posible cumplirla durante el periodo de su gestión?
+                                </h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="home-banner mt80">
+            <div class="container-fluid">
+                <div class="row text-center mb80 mt80">
+                    <div class="col-md-12 col-xs-12"> <img class="banner-logo" src="http://jaliscocomovamos.org/ofertodromo/img/ofertodromo-logo-yellow.svg"> </div>
+                </div>
+            </div>
+        </div>
+        <?php include('includes/bannerv2.php'); ?>
+        <?php include('includes/footer.php'); ?>
